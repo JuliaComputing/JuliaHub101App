f97e95f7a8bb0303ddfa3180e585b187b2db3e7a
Jacob Vaverka <jvaverka@protonmail.com>
2022-06-27 23:13:36 -0400

    fix: main run file for JH

--- bin/main.jl
+++ bin/main.jl
@@ -1,7 +1,20 @@
-push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
+@info "@__DIR__: " @__DIR__
+@info "pwd(): " pwd()
+
+ENV["HOST"] = "0.0.0.0"
+
+push!(LOAD_PATH, abspath(realpath(joinpath(@__DIR__, "../src/"))))
+push!(LOAD_PATH, abspath(realpath(joinpath(@__DIR__, ".."))))
+@info "LOAD_PATH: " LOAD_PATH
 
 ENV["STARTSERVER"] = true
 ENV["GENIE_ENV"] = "prod"
 ENV["EARLYBIND"] = true
-# hello world
-include("../bootstrap.jl")
+
+@info "haskey EARLYBIND" haskey(ENV, "EARLYBIND")
+@info "EARLYBIND" ENV["EARLYBIND"]
+
+bootpath = abspath(realpath(joinpath(@__DIR__, "..")))
+@info "Bootstrap path: " bootpath
+cd(bootpath)
+run(`julia --project=. bootstrap.jl`)
